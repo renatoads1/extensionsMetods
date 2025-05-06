c77df1c82cd71dee1201398b26abd429a22dcd93
renatoads1 <renato.lacerda>
2025-05-06 12:06:09 -0300

    delegate

--- extensionsMetods/Program.cs
+++ extensionsMetods/Program.cs
@@ -1,6 +1,36 @@
 ﻿
 
 using extensionsMetods.Extensions;
+using extensionsMetods.Services;
+namespace extensionsMetods
+{
+    delegate double deleg(double x, double y);
+    public class MyClass
+    {
 
-DateTime dt = new DateTime(2023, 10, 1, 12, 0, 0);
-Console.WriteLine(dt.ElapedTime());
+        public static void Main(string[] args)
+        {
+
+            //2ºparte
+            double x = 10;
+            double y = 20;
+            double max = CalcServices.Max(x, y);
+            double min = CalcServices.Min(x, y);
+            double sum = CalcServices.Sum(x, y);
+            double sub = CalcServices.Sub(x, y);
+
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Sub: {sub}");
+
+            deleg d = CalcServices.Sum;
+            Console.WriteLine($"Delegate Max: {d(x, y)}");
+
+            //1ºparte
+            DateTime dt = new DateTime(2023, 10, 1, 12, 0, 0);
+            Console.WriteLine(dt.ElapedTime());
+        }
+
+}
+}
---
+++ extensionsMetods/Services/CalcServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionsMetods.Services
+{
+    public class CalcServices
+    {
+        public static double Max(double x, double y) { 
+        
+            return (x > y) ? x : y;
+        }
+        public static double Min(double x, double y)
+        {
+
+            return (x < y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Sub(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
