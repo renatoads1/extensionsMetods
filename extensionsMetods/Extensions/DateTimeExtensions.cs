ab790e4ecc433573d90bc3bc959a2491885a8292
renatoads1 <renato.lacerda>
2025-05-06 11:55:17 -0300

    Adicionar arquivos de projeto.

---
+++ extensionsMetods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionsMetods.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ElapedTime( this DateTime thisDate) {
+
+            TimeSpan timeSpan = DateTime.Now - thisDate;
+            if (timeSpan.Days > 0)
+            {
+                return $"{timeSpan.Days} days ago";
+            }
+            else if (timeSpan.Hours > 0)
+            {
+                return $"{timeSpan.Hours} hours ago";
+            }
+            else if (timeSpan.Minutes > 0)
+            {
+                return $"{timeSpan.Minutes} minutes ago";
+            }
+            else if (timeSpan.Seconds > 0)
+            {
+                return $"{timeSpan.Seconds} seconds ago";
+            }
+            return "";
+        }
+    }
+}
---
+++ extensionsMetods/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+using extensionsMetods.Extensions;
+
+DateTime dt = new DateTime(2023, 10, 1, 12, 0, 0);
+Console.WriteLine(dt.ElapedTime());
